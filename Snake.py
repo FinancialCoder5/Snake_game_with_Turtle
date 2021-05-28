aee7babcf40ee3dbb2eae921e38165d79bc77f81
FinancialCoder5 <44364852+FinancialCoder5@users.noreply.github.com>
2021-05-28 14:26:46 +0530

    Add files via upload

---
+++ Snake.py
@@ -0,0 +1,62 @@
+from turtle import Turtle
+
+# Starting positions for the snake body
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        # for loop to to create three starting squares for the snake
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+       # add a new segment to the snake.
+
+
+    def move(self):
+        # This for loop is to make the 2nd and the 3rd snake body to follow no 1
+        # 3, 2, 1
+        # for seg_num in range(start=2, stop=0, step=1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # the segment or the body part at two is going to be the last segment.
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
